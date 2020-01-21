ffdc6c53d23b572b7c191433bffb85d0bc25c154
unknown <smazza@ucvts.org>
2020-01-20 22:51:37 -0500

    Finished consecutive

--- src/Exercises.java
+++ src/Exercises.java
@@ -222,9 +222,58 @@ public class Exercises {
 // Exercise 8 //
 
 	public boolean consecutive(int[] numbers) {
-		// write your code here
+
+		if (numbers.length < 3 || numbers == null) {
+
+			return false;
+
+		}
+
+  	int digitsInRow = 0
+		int remainder1 = -1
+		int remainder2 = -1
+
+		for (int i = 0; i < numbers.length; i++) {
+
+			remainder1 = numbers[i] % 2
+
+			if (remainder1 == 1) {
+
+				if (remainder2 == 1) {
+
+					digitsInRow++;
+
+				} else {
+
+					digitsInRow = 1;
+
+				}
+
+			} else {
+
+				if (remainder2 == 0) {
+
+					digitsInRow ++;
+
+				} else {
+
+					digitsInRow = 1;
+
+				}
+			}
+
+			if (digitsInRow == 3) {
+
+				return = 1;
+
+			}
+
+			remainder2 = remainder1;
+
+		}
 
 		return false;	// default return value to ensure compilation
+
 	}
 
 // Exercise 9 //
