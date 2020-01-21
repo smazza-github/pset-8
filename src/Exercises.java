a4979612ab6996daf88d1e15793639a2b49db0d4
unknown <smazza@ucvts.org>
2020-01-20 21:22:38 -0500

    Formatting

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,66 +1,87 @@
 public class Exercises {
 
+// Exercise 1 //
+
 	public boolean commonEnd(int[] a, int[] b) {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
 		}
-		
+
 		// write your code here
-		
+
 		return false;	// default return value to ensure compilation
 	}
-	
+
+// Exercise 2 //
+
 	public String[] endsMeet(String[] values, int n) {
 		// write your code here
-		
-		return null;	// default return value to ensure compilation
-	}
-	
-	public int difference(int[] numbers) {
-		// write your code here
-		
-		return -1;		// default return value to ensure compilation
-	}
-	
-	public double biggest(double[] numbers) {
-		// write your code here
-		
-		return -1;		// default return value to ensure compilation
-	}
-	
-	public String[] middle(String[] values) {
-		// write your code here
-		
+
 		return null;	// default return value to ensure compilation
 	}
 
-	public boolean increasing(int[] numbers) {
+// Exercise 3 //
+
+	public int difference(int[] numbers) {
 		// write your code here
-		
-		return false;	// default return value to ensure compilation
-	}
-	
-	public boolean everywhere(int[] numbers, int x) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
-	}
-	
-	public boolean consecutive(int[] numbers) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
-	}
-	
-	public boolean balance(int[] numbers) {
-		// write your code here
-		
-		return false;	// default return value to ensure compilation
-	}
-	
-	public int clumps(String[] values) {
-		// write your code here
-		
+
 		return -1;		// default return value to ensure compilation
 	}
+
+// Exercise 4 //
+
+	public double biggest(double[] numbers) {
+		// write your code here
+
+		return -1;		// default return value to ensure compilation
+	}
+
+// Exercise 5 //
+
+	public String[] middle(String[] values) {
+		// write your code here
+
+		return null;	// default return value to ensure compilation
+	}
+
+// Exercise 6 //
+
+	public boolean increasing(int[] numbers) {
+		// write your code here
+
+		return false;	// default return value to ensure compilation
+	}
+
+// Exercise 7 //
+
+	public boolean everywhere(int[] numbers, int x) {
+		// write your code here
+
+		return false;	// default return value to ensure compilation
+	}
+
+// Exercise 8 //
+
+	public boolean consecutive(int[] numbers) {
+		// write your code here
+
+		return false;	// default return value to ensure compilation
+	}
+
+// Exercise 9 //
+
+	public boolean balance(int[] numbers) {
+		// write your code here
+
+		return false;	// default return value to ensure compilation
+	}
+
+// Exercise 10 //
+
+	public int clumps(String[] values) {
+		// write your code here
+
+		return -1;		// default return value to ensure compilation
+	}
+	
 }
