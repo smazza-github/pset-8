309e74272051606095fc4e4b464fbdf0ac9c108f
unknown <smazza@ucvts.org>
2020-01-20 21:35:47 -0500

    Fixed missing }

--- src/Exercises.java
+++ src/Exercises.java
@@ -7,7 +7,7 @@ public class Exercises {
 
 			return false;
 
-		} else if ((a[a.length-1] == b[b.length-1]) || (a[0] == b[0]))
+		} else if ((a[a.length-1] == b[b.length-1]) || (a[0] == b[0])) {
 
 			return true;
 
